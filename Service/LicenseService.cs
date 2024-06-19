e2dd796c2086a2d751cc4688fc37a4bfd32379c1
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:42:51 +0200

    Created service to handle operations on licenses.

---
+++ Service/LicenseService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using s28201_Project.Context;
+using s28201_Project.Model;
+
+namespace s28201_Project.Service;
+
+public class LicenseService(ApiContext context)
+{
+    public async Task<SoftwareLicense?> GetLicenseByIdAsync(long id)
+    {
+        return await context.SoftwareLicenses.FirstOrDefaultAsync(c => c.SoftwareLicenseId == id);
+    }
+}
