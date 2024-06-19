14879a83c983af1678dface19a84f927cdfdb783
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:45:22 +0200

    Added generic params

--- Service/Client/IClientService.cs
+++ Service/Client/IClientService.cs
@@ -1,10 +1,13 @@
-﻿using s28201_Project.Model;
+﻿using s28201_Project.Dto;
+using s28201_Project.Model;
 
 namespace s28201_Project.Service;
 
-public interface IClientService
+public interface IClientService<TClient, TClientDto> 
+    where TClient : Client
+    where TClientDto : ClientDto
 {
-    public Task<bool> AddClientAsync(Client client);
-    public Task<bool> UpdateClientAsync(Client client);
-    public Task<bool> DeleteClientAsync(Client client);
+    public Task<bool> AddClientAsync(TClientDto dto);
+    public Task<bool> UpdateClientAsync(TClientDto dto);
+    public Task<TClient?> GetClientByIdAsync(long id);
 }
