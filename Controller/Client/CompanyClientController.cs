d9615fb09e83b43166e7781cab4da6f4072057fc
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:37:24 +0200

    Created and implemented get/add/update Company endpoints.

---
+++ Controller/Client/CompanyClientController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using s28201_Project.Dto;
+using s28201_Project.Service;
+
+namespace s28201_Project.Controller;
+
+[ApiController]
+[Route("api/clients/company/")]
+public class CompanyClientController(CompanyClientService service) : ControllerBase
+{
+    [HttpGet("{id:long}")]
+    public async Task<IActionResult> GetClientAsync(long id)
+    {
+        var client = await service.GetClientByIdAsync(id);
+        
+        if (client == null) return NotFound($"Individual Client with id[{id}] was not found.");
+
+        return Ok(client);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> AddClientAsync(CompanyClientDto dto)
+    {
+        var client = await service.AddClientAsync(dto);
+        
+        if (client == false) return BadRequest($"Unable to add client with KRS[{dto.KrsNum}]");
+        
+        return NoContent();
+    }
+    
+    [HttpPut]
+    public async Task<IActionResult> UpdateClientAsync(CompanyClientDto dto)
+    {
+        var result = await service.UpdateClientAsync(dto);
+
+        if (result == false) return BadRequest($"Unable to update client with KRS[{dto.KrsNum}]");
+        
+        return NoContent();
+    }
+}
