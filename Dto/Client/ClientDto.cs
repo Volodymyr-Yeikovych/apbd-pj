bcd267150dc6cc24bc9f0c524a2c80ca0533ab2a
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:36:16 +0200

    Added abstract Client dto class.

---
+++ Dto/Client/ClientDto.cs
@@ -0,0 +1,8 @@
+﻿namespace s28201_Project.Dto;
+
+public abstract class ClientDto
+{
+    public string Address { get; set; }
+    public string Email { get; set; }
+    public string PhoneNum { get; set; }
+}
