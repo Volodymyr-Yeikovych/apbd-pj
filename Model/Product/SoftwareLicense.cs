014df6b90e4dfbf1ae537d08eb9e9f4bb31e19c6
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:40:14 +0200

    Added EF TypeName for decimals, added many to one dependency for contracts.

--- Model/Product/SoftwareLicense.cs
+++ Model/Product/SoftwareLicense.cs
@@ -1,11 +1,20 @@
-﻿namespace s28201_Project.Model;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
-public class SoftwareLicense
+namespace s28201_Project.Model;
+
+public class SoftwareLicense : IProduct
 {
     public long SoftwareLicenseId { get; set; }
     public string Version { get; set; }
     public string Name { get; set; }
     public string Desc { get; set; }
+    [Column(TypeName = "decimal(36, 12)")]
+    public decimal StartingUpfrontPrice { get; set; }
+    [Column(TypeName = "decimal(36, 12)")]
+    public decimal StartingMonthlySubPrice { get; set; }
     public DistributionType DistributionType { get; set; }
     public SoftwareLicenseCategory LicenseCategory { get; set; }
+
+    public virtual List<CompanyContract> CompanyContracts { get; set; }
+    public virtual List<IndividualContract> IndividualContracts { get; set; }
 }
