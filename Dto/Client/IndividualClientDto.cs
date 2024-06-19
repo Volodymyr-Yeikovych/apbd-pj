9d7c97448da5f9b1fd2e2bb7ed154fff8f7cb1ba
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:38:11 +0200

    Added Dto

---
+++ Dto/Client/IndividualClientDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace s28201_Project.Dto;
+
+public class IndividualClientDto : ClientDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Pesel { get; init; }
+}
