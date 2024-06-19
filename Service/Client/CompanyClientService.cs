3c2d99a13b5c561106a02f41482616c5d119df41
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:46:00 +0200

    Implemented service to handle CRU operations on Company Client.

--- Service/Client/CompanyClientService.cs
+++ Service/Client/CompanyClientService.cs
@@ -1,24 +1,51 @@
-﻿using s28201_Project.Model;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using s28201_Project.Context;
+using s28201_Project.Dto;
+using s28201_Project.Model;
 
 namespace s28201_Project.Service;
 
-public class CompanyClientService : IClientService
+public class CompanyClientService(ApiContext context) : IClientService<CompanyClient, CompanyClientDto>
 {
-    // TODO: Not Implemented
-    public async Task<bool> AddClientAsync(Client client)
+    public async Task<bool> AddClientAsync(CompanyClientDto dto)
     {
-        return false;
+        var byKrs = await GetClientByKrsAsync(dto.KrsNum);
+        if (byKrs != null) return false;
+        
+        var client = new CompanyClient(dto);
+        
+        await context.CompanyClients.AddAsync(client);
+        await context.SaveChangesAsync();
+        
+        return true;
+    }
+    
+    public async Task<bool> UpdateClientAsync(CompanyClientDto dto)
+    {
+        var byKrs = await GetClientByKrsAsync(dto.KrsNum);
+        if (byKrs == null) return false;
+
+        byKrs.Set(dto);
+        
+        await context.SaveChangesAsync();
+        
+        return true;
     }
 
-    // TODO: Not Implemented
-    public async Task<bool> UpdateClientAsync(Client client)
+    public async Task<CompanyClient?> GetClientByIdAsync(long id)
     {
-        return false;
+        return await context.CompanyClients.FirstOrDefaultAsync(c => c.ClientId == id);
     }
 
-    // TODO: Not Implemented
-    public async Task<bool> DeleteClientAsync(Client client)
+    public async Task<CompanyClient?> GetClientByKrsAsync(string krs)
     {
-        return false;
+        return await context.CompanyClients.FirstOrDefaultAsync(c => c.KrsNum == krs);
+    }
+
+    public async Task<decimal> GetMaximalDiscountAsync(CompanyClient client)
+    {
+        decimal max = client.Discounts.IsNullOrEmpty() ? 0 : await context.Discounts.Select(d => d.ValuePercent).MaxAsync();
+        return max;
     }
 }
