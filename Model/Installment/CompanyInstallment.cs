661e5886f2f128359f0fb06dc4299871c6520e1f
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:38:54 +0200

    Added model class.

---
+++ Model/Installment/CompanyInstallment.cs
@@ -0,0 +1,7 @@
+﻿namespace s28201_Project.Model;
+
+public class CompanyInstallment : Installment
+{
+    public long CompanyContractId { get; set; }
+    public virtual CompanyContract CompanyContract { get; set; }
+}
