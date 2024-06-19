8c9c6ba218daa02ba6e80a62be0d75635adab2f5
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:38:16 +0200

    Added Dto

---
+++ Dto/Product/SoftwareLicenseDto.cs
@@ -0,0 +1,9 @@
+﻿using s28201_Project.Model;
+
+namespace s28201_Project.Dto;
+
+public class SoftwareLicenseDto
+{
+    public long LicenseId { get; set; }
+    public string Version { get; set; }
+}
