04dd9b82b0ae45a0b67cf67ce28cbc7682e8cd0a
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:44:57 +0200

    Added Constructor from dto and Set from dto method

--- Model/Client/IndividualClient.cs
+++ Model/Client/IndividualClient.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.IdentityModel.Tokens;
+using s28201_Project.Dto;
 
 namespace s28201_Project.Model;
 
@@ -6,17 +7,33 @@ public class IndividualClient : Client
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string Pesel {
-        get => Pesel;
-        set
-        {
-            if (Pesel.IsNullOrEmpty())
-            {
-                Pesel = value;
-            }
-        }
-    }
+    public string Pesel { get; init; }
+
     public bool IsDeleted { get; set; }
     
     public virtual List<Discount> Discounts { get; set; }
+    public virtual List<IndividualContract> Contracts { get; set; }
+
+    public IndividualClient()
+    {
+    }
+
+    public IndividualClient(IndividualClientDto dto)
+    {
+        Address = dto.Address;
+        Email = dto.Email;
+        PhoneNum = dto.PhoneNum;
+        FirstName = dto.FirstName;
+        LastName = dto.LastName;
+        Pesel = dto.Pesel;
+    }
+
+    public void Set(IndividualClientDto dto)
+    {
+        Address = dto.Address;
+        Email = dto.Email;
+        PhoneNum = dto.PhoneNum;
+        FirstName = dto.FirstName;
+        LastName = dto.LastName;
+    }
 }
