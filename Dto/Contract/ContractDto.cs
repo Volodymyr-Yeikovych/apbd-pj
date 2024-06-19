ef168097c2be6ead0b88ca8e774a217954bb1628
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:38:01 +0200

    Added Dto

---
+++ Dto/Contract/ContractDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace s28201_Project.Dto;
+
+public class ContractDto
+{
+    public string ClientIdentifier { get; set; }
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    public int AdditionalSupportYears { get; set; }
+
+    public SoftwareLicenseDto License { get; set; }
+}
