ed1998be1e8e4ae3566454ca567ebe6ee45216f7
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:34:41 +0200

    Removed deprecated class.

--- Model/Discount/IndividualDiscount.cs
+++
@@ -1,8 +0,0 @@
-﻿// namespace s28201_Project.Model;
-//
-// public class IndividualDiscount : Discount
-// {
-//     public long IndividualId { get; set; }
-//     public virtual IndividualClient Client { get; set; }
-//
-// }
