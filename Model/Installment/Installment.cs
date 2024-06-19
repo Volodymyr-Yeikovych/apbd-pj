2d2880c43dc30390a101386ca4f963fec73ecf5f
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:40:44 +0200

    Made abstract added EF TypeName for decimals.

--- Model/Installment/Installment.cs
+++ Model/Installment/Installment.cs
@@ -1,13 +1,16 @@
-﻿namespace s28201_Project.Model;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
-public class Installment
+namespace s28201_Project.Model;
+
+public abstract class Installment
 {
     public long InstallmentId { get; set; }
+    [Column(TypeName = "decimal(36, 12)")]
     public decimal Price { get; set; }
     public DateOnly DatePaid { get; set; }
     public DateOnly? DateConfirmed { get; set; }
     public string? Desc { get; set; }
-
+    
     public void Confirm()
     {
         DateConfirmed = DateOnly.FromDateTime(DateTime.Now);
