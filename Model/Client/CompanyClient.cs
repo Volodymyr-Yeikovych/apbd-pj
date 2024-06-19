c02f82798eda8d071b1a435ebab144e35adfca09
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:36:48 +0200

    Fixed bug and added Constructor from dto and Set from dto method.

--- Model/Client/CompanyClient.cs
+++ Model/Client/CompanyClient.cs
@@ -1,21 +1,34 @@
 ﻿using Microsoft.IdentityModel.Tokens;
+using s28201_Project.Dto;
 
 namespace s28201_Project.Model;
 
 public class CompanyClient : Client
 {
     public string Name { get; set; }
-    public string KrsNum
-    {
-        get => KrsNum;
-        set
-        {
-            if (KrsNum.IsNullOrEmpty())
-            {
-                KrsNum = value;
-            }
-        }
-    }
+    public string KrsNum { get; init; }
     
     public virtual List<Discount> Discounts { get; set; }
+    public virtual List<CompanyContract> Contracts { get; set; }
+    
+    public CompanyClient()
+    {
+    }
+
+    public CompanyClient(CompanyClientDto dto)
+    {
+        Address = dto.Address;
+        Email = dto.Email;
+        PhoneNum = dto.PhoneNum;
+        Name = dto.Name;
+        KrsNum = dto.KrsNum;
+    }
+
+    public void Set(CompanyClientDto dto)
+    {
+        Address = dto.Address;
+        Email = dto.Email;
+        PhoneNum = dto.PhoneNum;
+        Name = dto.Name;
+    }
 }
