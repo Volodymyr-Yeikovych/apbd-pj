945a39c85abc8b1345cb59a6d25d3409452c27cf
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:38:47 +0200

    Added model class.

---
+++ Model/Contract/CompanyContract.cs
@@ -0,0 +1,8 @@
+﻿namespace s28201_Project.Model;
+
+public class CompanyContract : Contract
+{
+    public long CompanyId { get; set; }
+    public virtual CompanyClient CompanyClient { get; set; }
+    public virtual List<CompanyInstallment> CorporateInstallments { get; set; }
+}
