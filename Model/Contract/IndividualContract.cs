76fa38a81b8233c195c7bd5054b5a6e02a79c2e5
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:43:08 +0200

    Created model.

---
+++ Model/Contract/IndividualContract.cs
@@ -0,0 +1,8 @@
+﻿namespace s28201_Project.Model;
+
+public class IndividualContract : Contract
+{
+    public long IndividualId { get; set; }
+    public virtual IndividualClient IndividualClient { get; set; }
+    public virtual List<IndividualInstallment> IndividualInstallments { get; set; }
+}
