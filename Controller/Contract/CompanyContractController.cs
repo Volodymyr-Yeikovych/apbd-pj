2e7afcd1a9a485fe3a3e8b5a50fd7fa9b645f75b
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-19 15:41:44 +0200

    Created and implemented Get and Add Contract endpoints.

---
+++ Controller/Contract/CompanyContractController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using s28201_Project.Dto;
+using s28201_Project.Service;
+
+namespace s28201_Project.Controller;
+
+[ApiController]
+[Route("api/contracts/company/")]
+public class CompanyContractController(CompanyContractService service) : ControllerBase
+{
+    [HttpGet("{id:long}")]
+    public async Task<IActionResult> GetContractAsync(long id)
+    {
+        var client = await service.GetContractByIdAsync(id);
+        
+        if (client == null) return NotFound($"Contract with id[{id}] was not found.");
+
+        return Ok(client);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> AddContractAsync(ContractDto dto)
+    {
+        var response = await service.AddContractAsync(dto);
+
+        if (!response.IsSuccess) return BadRequest(response.Message);
+        
+        return NoContent();
+    }
+    
+    // TODO: Not Implemented
+    [HttpPut]
+    public async Task<IActionResult> SignContractAsync()
+    {
+        return NoContent();
+    }
+}
