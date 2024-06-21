9b805a727437f22077916ba1161de698059e7bb7
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-21 13:20:29 +0200

    Added CompanyInstallmentService to Scoped.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using s28201_Project.Context;
 using s28201_Project.Service;
+using s28201_Project.Service.Installment;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ builder.Services.AddScoped<IndividualClientService>();
 builder.Services.AddScoped<CompanyClientService>();
 builder.Services.AddScoped<CompanyContractService>();
 builder.Services.AddScoped<LicenseService>();
+builder.Services.AddScoped<CompanyInstallmentService>();
 
 var app = builder.Build();
 
