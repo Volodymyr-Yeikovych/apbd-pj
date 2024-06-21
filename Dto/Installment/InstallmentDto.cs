c12415e30303c87a0f404382c01217ee89e7dea8
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-21 13:21:04 +0200

    Added dto class.

---
+++ Dto/Installment/InstallmentDto.cs
@@ -0,0 +1,11 @@
+﻿using s28201_Project.Model;
+
+namespace s28201_Project.Dto;
+
+public abstract class InstallmentDto
+{
+    public DistributionType DistributionType { get; set; }
+    public decimal Price { get; set; }
+    public DateOnly DatePaid { get; set; }
+    public string? Desc { get; set; }
+}
