98f9dfddce0b81cc66c8a10def99e06be0b0492b
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-21 13:22:06 +0200

    Added controller to handle company installment operations.

---
+++ Controller/Installment/CompanyInstallmentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using s28201_Project.Dto;
+using s28201_Project.Service.Installment;
+
+namespace s28201_Project.Controller;
+
+[ApiController]
+[Route("api/payments/company")]
+public class CompanyInstallmentController(CompanyInstallmentService service) : ControllerBase
+{
+    // TODO: Not Implemented
+    [HttpPost]
+    public async Task<IActionResult> AddInstallmentAsync(CompanyInstallmentDto dto)
+    {
+        var response = await service.ProcessInstallmentAsync(dto);
+
+        if (!response.IsSuccessful) return BadRequest(response.message);
+        
+        return NoContent();
+    }
+}
