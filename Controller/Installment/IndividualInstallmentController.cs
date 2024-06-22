a703ffba545d35e01133df55b462a1b38e67134a
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-22 17:39:23 +0200

    Added Authorisation roles. Implemented logic.

---
+++ Controller/Installment/IndividualInstallmentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using s28201_Project.Dto;
+using s28201_Project.Service.Installment;
+
+namespace s28201_Project.Controller;
+
+[ApiController]
+[Route("api/payments/individual/")]
+public class IndividualInstallmentController(IndividualInstallmentService service) : ControllerBase
+{
+    [HttpPost]
+    [Authorize(Policy = "UserOrAdmin")]
+    public async Task<IActionResult> AddInstallmentAsync(IndividualInstallmentDto dto)
+    {
+        var response = await service.ProcessInstallmentAsync(dto);
+
+        if (!response.IsSuccessful) return BadRequest(response.Message);
+        
+        return NoContent();
+    }
+}
