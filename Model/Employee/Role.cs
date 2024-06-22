edfbc9a44563076fc2b884a4b3dd8e0fe8b79832
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-22 17:41:33 +0200

    Added Employee role class.

---
+++ Model/Employee/Role.cs
@@ -0,0 +1,26 @@
+﻿namespace s28201_Project.Model.Employee;
+
+public enum Role
+{
+    Admin,
+    User,
+    NoRole
+}
+
+public static class RoleExtensions
+{
+    public static string ToIndexString(this Role value)
+    {
+        switch (value)
+        {
+            case Role.Admin:
+                return "0";
+            case Role.User:
+                return "1";
+            case Role.NoRole:
+                return "2";
+            default:
+                throw new ArgumentOutOfRangeException(nameof(value), value, null);
+        }
+    }
+}
