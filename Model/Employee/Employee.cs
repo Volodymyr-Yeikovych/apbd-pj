21228c695da1af71aec061d4496ab36fef4ece3e
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-22 17:40:47 +0200

    Added Model

---
+++ Model/Employee/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace s28201_Project.Model.Employee;
+
+public class Employee
+{
+    public long EmployeeId { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public Role Role { get; set; }
+}
