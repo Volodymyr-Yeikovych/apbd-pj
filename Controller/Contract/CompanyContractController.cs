e43b7ee0e9e77cad25871e50117bf43c4556a63f
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-22 17:38:49 +0200

    Added Authorisation roles. Implemented sign contract.

--- Controller/Contract/CompanyContractController.cs
+++ Controller/Contract/CompanyContractController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using s28201_Project.Dto;
 using s28201_Project.Service;
 
@@ -9,6 +10,7 @@ namespace s28201_Project.Controller;
 public class CompanyContractController(CompanyContractService service) : ControllerBase
 {
     [HttpGet("{id:long}")]
+    [Authorize(Policy = "UserOrAdmin")]
     public async Task<IActionResult> GetContractAsync(long id)
     {
         var client = await service.GetContractByIdAsync(id);
@@ -19,6 +21,7 @@ public class CompanyContractController(CompanyContractService service) : Control
     }
     
     [HttpPost]
+    [Authorize(Policy = "UserOrAdmin")]
     public async Task<IActionResult> AddContractAsync(ContractDto dto)
     {
         var response = await service.AddContractAsync(dto);
@@ -28,15 +31,15 @@ public class CompanyContractController(CompanyContractService service) : Control
         return NoContent();
     }
     
-    // TODO: Not Implemented
-    // [HttpPut("{id:long}")]
-    // public async Task<IActionResult> SignContractAsync(long id)
-    // {
-    //     var isSigned = await service.SignContractAsync(id);
-    //
-    //     if (!isSigned) return BadRequest($"Contract with id[{id}] cannot be signed.");
-    //      
-    //     return NoContent();
-    // }
+    [HttpPut("{id:long}")]
+    [Authorize(Policy = "Admin")]
+    public async Task<IActionResult> SignContractAsync(long id)
+    {
+        var isSigned = await service.TrySignContractAsync(id);
+    
+        if (!isSigned) return BadRequest($"Failed to sign contract with id[{id}].");
+         
+        return NoContent();
+    }
     
 }
