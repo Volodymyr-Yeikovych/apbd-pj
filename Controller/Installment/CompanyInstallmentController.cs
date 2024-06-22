91c119419339ef30c1558dd00a53985c558e57e1
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-06-22 17:38:55 +0200

    Added Authorisation roles.

--- Controller/Installment/CompanyInstallmentController.cs
+++ Controller/Installment/CompanyInstallmentController.cs
@@ -1,20 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using s28201_Project.Dto;
 using s28201_Project.Service.Installment;
 
 namespace s28201_Project.Controller;
 
 [ApiController]
-[Route("api/payments/company")]
+[Route("api/payments/company/")]
 public class CompanyInstallmentController(CompanyInstallmentService service) : ControllerBase
 {
-    // TODO: Not Implemented
     [HttpPost]
+    [Authorize(Policy = "UserOrAdmin")]
     public async Task<IActionResult> AddInstallmentAsync(CompanyInstallmentDto dto)
     {
         var response = await service.ProcessInstallmentAsync(dto);
 
-        if (!response.IsSuccessful) return BadRequest(response.message);
+        if (!response.IsSuccessful) return BadRequest(response.Message);
         
         return NoContent();
     }
